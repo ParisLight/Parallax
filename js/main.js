d178df6f474c6d5d675462aeca4ae1f1bf16f8ec
Константин Павленко <space20144@mail.ru>
2023-01-21 17:31:48 +0300

    добавил аудио

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-new Swiper('.swiper', {
-    parallax: true,
-    mousewheel: true,
-    spaceBetween: 30,
-    speed: 2000,
-})
+new Swiper(".swiper", {
+  parallax: true,
+  mousewheel: true,
+  spaceBetween: 30,
+  speed: 2000,
+});
 
-document.addEventListener('DOMContentLoaded', function(e) {
-        document.querySelectorAll('.preloader__img').forEach(element => {
-            element.classList.add('preloader__img-active');
-            element.addEventListener('transitionend', function(e){
-                document.querySelector('.preloader').classList.add('preloader-out');
-
-            })
-        });
-        setTimeout(() => {
-            document.querySelector('.preloader').remove()
-        }, 7000);
-})
+document.addEventListener("DOMContentLoaded", function (e) {
+  document.querySelector(".play").addEventListener("click", (e) => {
+    document.querySelector(".play").remove();
+    document.querySelectorAll(".preloader__img").forEach((element) => {
+      element.classList.add("preloader__img-active");
+      element.addEventListener("transitionend", function (e) {
+        document.querySelector(".preloader").classList.add("preloader-out");
+      });
+    });
+    setTimeout(() => {
+      document.querySelector(".preloader").remove();
+    }, 7000);
+  });
+});
